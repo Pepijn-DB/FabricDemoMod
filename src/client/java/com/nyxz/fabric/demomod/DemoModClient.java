17e1c32d2534d6500a9969dd9c2b326846e34ebc
pepijn <pepijn@nyxz.nl>
2025-12-05 23:35:33 +0100

    Template project

---
+++ src/client/java/com/nyxz/fabric/demomod/DemoModClient.java
@@ -0,0 +1,10 @@
+package com.nyxz.fabric.demomod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class DemoModClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
